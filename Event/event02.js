344dd94188f4e0af88a1d9125138615653d2c0ca
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-08-27 20:13:25 +0530

    Add files via upload

---
+++ Event/event02.js
@@ -0,0 +1,14 @@
+const EventEmitter = require('events');
+const eventEmitter = new EventEmitter();
+
+eventEmitter.on('greet', () => {
+    console.log("First listener");
+})
+
+eventEmitter.on('greet', () => {
+    console.log("Second listener");
+})
+
+eventEmitter.emit('greet');
+
+console.log(eventEmitter.listeners('greet'));

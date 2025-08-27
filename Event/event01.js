c5487d02195a31519b431ec742486fa74ac142ea
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-08-27 20:10:34 +0530

    Add files via upload

---
+++ Event/event01.js
@@ -0,0 +1,11 @@
+const EventEmitter = require("events")
+const eventEmitter = new EventEmitter();
+
+eventEmitter.once("notification", () => {
+    console.log("u r hacked");
+})
+
+eventEmitter.emit("notification");
+eventEmitter.emit("notification");
+eventEmitter.emit("notification");
+eventEmitter.emit("notification");

db96b428989a92e8ff3f02aa0890549edca6e6b3
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-10-09 21:16:26 +0530

    Update event01.js

--- Event/event01.js
+++ Event/event01.js
@@ -2,8 +2,7 @@ const EventEmitter = require("events")
 const eventEmitter = new EventEmitter();
 eventEmitter.once("notification", () => {
     console.log("u r hacked");
-})
-
+})
 eventEmitter.emit("notification");
 
 eventEmitter.emit("notification");

5f8d74f7fc85d4f6860aae2e6ce64b8cac4d7865
Adrima Biswas <adrimabiswas2003@gmail.com>
2025-10-15 23:51:56 +0530

    Update event00.js

--- Event/event00.js
+++ Event/event00.js
@@ -3,6 +3,7 @@ const EventEmitter = require('events');
 const eventEmitter = new EventEmitter();
 
 eventEmitter.on('greet', (n) => {
+
     console.log("First event",n);
 })
 eventEmitter.emit('greet',900);
